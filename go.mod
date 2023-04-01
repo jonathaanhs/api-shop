ce68a196637b8a9efd079ba1ec35ee7aa2321a3d
Jonathan <silalahiboo@gmail.com>
2023-04-01 13:32:35 +0700

    feat: add env config infra

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/jmoiron/sqlx v1.3.5
+	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/lib/pq v1.2.0
 	github.com/sirupsen/logrus v1.9.0
 	github.com/stretchr/testify v1.8.2
---
+++ internal/infra/envcfg.go
@@ -0,0 +1,17 @@
+package infra
+
+import (
+	"fmt"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+func LoadPgDatabaseCfg() (*DatabaseCfg, error) {
+	var cfg DatabaseCfg
+	prefix := "PG"
+	if err := envconfig.Process(prefix, &cfg); err != nil {
+		return nil, fmt.Errorf("%s: %w", prefix, err)
+	}
+
+	return &cfg, nil
+}
